20fdc2c5f512226aed22e41b499c13e36a64f9bf
Gichuki Muchiri <119453651+GichukiM@users.noreply.github.com>
2024-11-13 15:16:37 +0300

    Delete backend-Password-Generator directory

--- backend-Password-Generator/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
-
-router.post("/signup", authController.signupWithEmail);
-router.get("/auth/google", authController.oauthSignup);
-router.get("/auth/github", authController.oauthSignup);
-
-module.exports = router;
